5f3d5ac3409de7ef05f4a5de82490d9d74b1c10a
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-04-09 08:04:26 +0530

    Count digits optimized solutions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// countDigits;
+// countDigits  O(n);
 
 void countDigits(int n){
     int count = 0;
@@ -13,9 +13,26 @@ void countDigits(int n){
     cout<<"Number of digits : "<<count;
 }
 
+//Count digits O(1)
+void countDigits1(int n){
+    string str = to_string(n);
+    cout<<str.length()<<endl;
+}
+
+// Count Digits Most optimized  log10 base n
+void countDigits2(int n){
+    int digits = floor(log10(n) + 1);
+    cout<<digits<<" ";
+}
+
+
+// Reverse a given number
+
+
+
 
 
 int main(){
-    countDigits(123410);
+    countDigits1(1234310);
     return 0;
 }
